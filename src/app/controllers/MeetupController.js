9136e31662702bbd34944fcefc77e6d1d3a82e64
marvin3w <marvin.3w@gmail.com>
2019-09-10 03:26:21 -0300

    Create meetup table and route store to create meetup

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,34 @@
+import * as Yup from 'yup';
+import { parseISO, isBefore } from 'date-fns';
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      file_id: Yup.number().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const user_id = req.userId;
+
+    const meetup = await Meetup.create({
+      ...req.body,
+      user_id,
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import FilesController from './app/controllers/FilesController';
+import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,6 +19,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FilesController.store);
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/meetups', MeetupController.store);
 
 export default routes;
