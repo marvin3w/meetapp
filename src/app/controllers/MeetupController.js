763c4f7cf7984c0d12ba86b397f57e49c8cb76d7
marvin3w <marvin.3w@gmail.com>
2019-09-11 02:01:41 -0300

    Update and Delete Meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -29,6 +29,56 @@ class MeetupController {
 
     return res.json(meetup);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      file_id: Yup.number(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({ error: 'Not authorized.' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't update past meetups." });
+    }
+
+    await meetup.update({ ...req.body });
+
+    return res.json(meetup);
+  }
+
+  async index(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async delete(req, res) {
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permissions to cancel this appointament.",
+      });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't delete past meetups." });
+    }
+
+    meetup.destroy();
+
+    return res.status(200).json({ success: 'Successfully deleted Meetup.' });
+  }
 }
 
 export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -22,5 +22,8 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/meetups', MeetupController.store);
+routes.get('/meetups', MeetupController.index);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
 
 export default routes;
