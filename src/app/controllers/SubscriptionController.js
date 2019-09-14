b3729dc5a20b1bd921f01bb8ab97d064952811e1
marvin3w <marvin.3w@gmail.com>
2019-09-14 12:23:29 -0300

    Validate subscription

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -13,6 +13,33 @@ class SubscriptionController {
         .json({ error: 'The event creator cannot register for it' });
     }
 
+    if (meetup.past) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot sign up for previous meetings.' });
+    }
+
+    const checkDate = await Subscription.findOne({
+      where: {
+        user_id: user.id,
+      },
+      include: [
+        {
+          model: Meetup,
+          required: true,
+          where: {
+            date: meetup.date,
+          },
+        },
+      ],
+    });
+
+    if (checkDate) {
+      return res
+        .status(400)
+        .json({ error: "Can't subscribe to two meetups at the same time" });
+    }
+
     const subscription = await Subscription.create({
       user_id: user.id,
       meetup_id: meetup.id,
